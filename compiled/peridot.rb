0dcc6351bb2ffe561cf772010bcb21507cdcc662
Ondrej Bilka <neleai@seznam.cz>
2011-10-20 08:58:19 +0200

    adding numbers

--- compiled/peridot.rb
+++ compiled/peridot.rb
@@ -1,19 +1,22 @@
 class Peridot_parser < Amethyst
 
+def expr_Peridot_parsercb_1(bind)
+bind[0]+bind[1]
+end
 
 end
 
 
 def peridot_compiled_by
-'3dc91be876530e8b8365e27304be8d79'
+'32e1bf03e0f843f0f8290a1271d8023d'
 end
 def peridot_source_hash
-'309209eef611d1d3031238c3e94690fc'
+'1c590ac89bbbc65b29e054dbc3ad1527'
 end
 def testversionperidot(r)
  raise "invalid version" if r!=peridot_version
 end
 def peridot_version
-'f74c84b358233ef07d602908a320c090'
+'cfb697db0d9e18eab06f1ac8c69afc26'
 end
   require 'compiled/peridot_c'
--- compiled/peridot_c.c
+++ compiled/peridot_c.c
@@ -3,12 +3,16 @@ VALUE cls_Peridot_parser;
 VALUE Peridot_parser_expr(VALUE self );
 VALUE Peridot_parser_root(VALUE self );
 VALUE c_1;
-VALUE c_2;
+VALUE s_expr_Peridot_parsercb_1Peridot_parser;
+VALUE s_numberPeridot_parser;
 VALUE s_tokenPeridot_parser;
-VALUE Peridot_parser_expr(VALUE self ){VALUE vals[0]; VALUE bind=bind_new2(1);  int x;VALUE arg0,arg1,arg2,arg3;VALUE it;
+VALUE Peridot_parser_expr(VALUE self ){VALUE vals[0]; VALUE bind=bind_new2(3);  int x;VALUE arg0,arg1,arg2,arg3;VALUE it;
 
- it=rb_obj_clone(c_1); arg0=it; it=rb_funcall(self,s_tokenPeridot_parser,1,arg0); FAILTEST(fail);it=c_2;
- bind_aset(bind,0/*_result*/,it); it=bind_aget(bind,0/*_result*/);
+  it=rb_funcall(self,s_numberPeridot_parser,0); FAILTEST(fail);
+ bind_aset(bind,0/*a*/,it); int oldpos1; while(1){oldpos1=ame_getpos(self);  it=rb_obj_clone(c_1); arg0=it; it=rb_funcall(self,s_tokenPeridot_parser,1,arg0); FAILTEST(break1);  it=rb_funcall(self,s_numberPeridot_parser,0); FAILTEST(break1);
+ bind_aset(bind,1/*b*/,it); it=rb_funcall(self,s_expr_Peridot_parsercb_1Peridot_parser,1,bind); 
+ bind_aset(bind,0/*a*/,it);  if (ame_getstop(self)!=Qnil){{oldpos1=ame_getpos(self);goto break1;} } } break1: ame_setstop(self,Qnil);  ame_setpos(self,oldpos1); it=bind_aget(bind,0/*a*/);
+ bind_aset(bind,2/*_result*/,it); it=bind_aget(bind,2/*_result*/);
 return it;
 fail: return failobj; }
 VALUE Peridot_parser_root(VALUE self ){VALUE vals[0]; VALUE bind=bind_new2(1);  int x;VALUE arg0,arg1,arg2,arg3;VALUE it;
@@ -20,9 +24,10 @@ fail: return failobj; }
 void Init_peridot_c(){ 
  cls_Peridot_parser=rb_define_class("Peridot_parser",rb_const_get(rb_cObject,rb_intern("Amethyst"))); 
 failobj=rb_eval_string("FAIL");
-c_1=rb_str_new2("2+2");rb_ary_push(rb_const_get(rb_cObject,rb_intern("GC_mark")),c_1);
-c_2=rb_funcall(rb_str_new2("42"),rb_intern("to_i"),0);rb_ary_push(rb_const_get(rb_cObject,rb_intern("GC_mark")),c_2);
+c_1=rb_str_new2("+");rb_ary_push(rb_const_get(rb_cObject,rb_intern("GC_mark")),c_1);
+s_expr_Peridot_parsercb_1Peridot_parser=rb_intern("expr_Peridot_parsercb_1");
+s_numberPeridot_parser=rb_intern("number");
 s_tokenPeridot_parser=rb_intern("token");
 rb_define_method(cls_Peridot_parser,"expr",Peridot_parser_expr,0);
 rb_define_method(cls_Peridot_parser,"root",Peridot_parser_root,0);
- rb_eval_string("testversionperidot('f74c84b358233ef07d602908a320c090')");}
+ rb_eval_string("testversionperidot('cfb697db0d9e18eab06f1ac8c69afc26')");}
