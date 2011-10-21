2d75c38eeb28e80ab064a69ba1593dc3f2af1aa2
Ondrej Bilka <neleai@seznam.cz>
2011-10-21 10:24:28 +0200

    sample translator

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,9 +1,11 @@
 require '../amethyst/lib/amethyst'
-
-["peridot"].each{|f|
+require 'lib/ast'
+["peridot","translator"].each{|f|
 Compiler::compile("amethyst/#{f}.ame","compiled/#{f}.rb",f)
+
+require "compiled/#{f}"
 }
 
-require 'compiled/peridot'
 
 puts Peridot_parser.new.parse(:root,"2+2")
+puts Peridot_translator.new.parse(:root,[Func[{:name=>"plus",:ary=>[Var[{:name=>"a"}],Var[{:name=>"b"}]]}]])
