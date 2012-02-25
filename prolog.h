98460775249934d8405235f3ca186a952f3d0ad5
Ondrej Bilka <neleai@seznam.cz>
2012-02-25 22:00:12 +0100

    preparing implicit self

--- prolog.h
+++ prolog.h
@@ -12,6 +12,7 @@ typedef obj (* obj_fn)();
 obj Int(int i)  {obj_fixint *o=malloc(sizeof(obj_fixint));  o->class=0;o->in=i;                   return o;}
 obj Array()     {obj_array  *o=malloc(sizeof(obj_array ));  o->class=1;o->ary=(obj *) malloc(100);return o;}
 obj Str(char *c){obj_string *o=malloc(sizeof(obj_string));  o->class=2;o->ary=c;                  return o;}
+obj Toplevel()  {_obj       *o=malloc(sizeof(_obj));        o->class=3;                           return o;}
 
 int   obj2int(obj o){return ((obj_fixint*)o)->in;}
 obj*  obj2ary(obj o){return ((obj_array *)o)->ary;}
