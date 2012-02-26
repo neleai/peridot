b22481560ce71c7388d6e6de67ed077c9bdb49fb
Ondrej Bilka <neleai@seznam.cz>
2012-02-26 19:53:02 +0100

    loading directly into ruby

--- bootstrap.rb
+++ bootstrap.rb
@@ -22,5 +22,7 @@ File.open("test.c","w"){|f|
  f.puts Peridot_translator.new.parse(:root,p) 
 }
 puts `gcc test.c -shared -fPIC -o test.so`
-puts `gcc load.c -ldl` 
-puts `./a.out`
+puts `gcc -I. -I/usr/include/ruby-1.9.1/x86_64-linux -I/usr/include/ruby-1.9.1/ruby/backward -I/usr/include/ruby-1.9.1 -I. -fPIC -fno-strict-aliasing -g -fpic load.c -c -o load.o`
+puts `gcc -shared -o Load.so load.o -L. -L/usr/lib -L. -rdynamic -Wl,-export-dynamic -lruby-1.9.1 -lpthread -lrt -ldl -lcrypt -lm -lc`
+require 'Load'
+load_test
--- load.c
+++ load.c
@@ -1,6 +1,11 @@
+#include "ruby.h"
 #include <dlfcn.h>
-int main(){
+VALUE load_test(){
   void *testl=dlopen("./test.so",RTLD_NOW);
   void (*man)()=dlsym(testl,"main");
   man();
+  return Qnil;
+}
+int Init_Load(){
+  rb_define_method(rb_cObject,"load_test",load_test,0);
 }
