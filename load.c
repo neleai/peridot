b1d1bd4d50d931b96ca47ddbd7d8778230873980
Ondrej Bilka <neleai@seznam.cz>
2012-03-03 17:18:20 +0100

    use classes to resolve methods

--- load.c
+++ load.c
@@ -1,4 +1,5 @@
 #include "ruby.h"
+#include "prolog.h"
 #include <dlfcn.h>
 
 void *sl;
@@ -9,28 +10,36 @@ VALUE load_test(){
 }
 
 void** methods[100];
-void add_method(int clas,int num,char *name){
-  methods[clas][num]=dlsym(sl,name);
-}
-VALUE add_methodrb(VALUE self,VALUE clas,VALUE num,VALUE name){
-  add_method(FIX2INT(clas),FIX2INT(num),RSTRING_PTR(name));
-  return Qnil;
-}
 VALUE set_sl(VALUE self,VALUE lib){
   sl=dlopen(RSTRING_PTR(lib),RTLD_NOW);
   return Qnil;
 }
-char *classes[100];int clasno;
+char *class_names[100];int clasno;
+obj_Class class_obj[100];
 VALUE add_class(VALUE self,VALUE name){int i;
   char *clasname=RSTRING_PTR(name);
+  if (!clasno&& strcmp(clasname,"Class")) add_class(self,rb_str_new2("Class"));
   for (i=0;i<clasno;i++)
-    if (!strcmp(clasname,classes[i])) return INT2FIX(i);
-  classes[clasno]=strdup(clasname);
+    if (!strcmp(clasname,class_names[i])) return INT2FIX(i);
+  class_names[clasno]=strdup(clasname);
   methods[clasno]=calloc(sizeof(void *),100);
+  class_obj[clasno]=Class(clasname);
   clasno++;
   return INT2FIX(clasno-1);
 }
+obj_Class class_named(char *s){
+  int no=FIX2INT(add_class(rb_cObject,rb_str_new2(s)));
+  return class_obj[no];
+}
 
+void add_method(int clas,int num,char *name){
+  class_obj[clas]->methods[num]=dlsym(sl,name);
+}
+
+VALUE add_methodrb(VALUE self,VALUE clas,VALUE num,VALUE name){
+  add_method(FIX2INT(clas),FIX2INT(num),RSTRING_PTR(name));
+  return Qnil;
+}
 void peridot_eval(char *s){
   rb_funcall(rb_cObject,rb_intern("peridot_eval"),1,rb_str_new2(s));
 }
--- prolog.h
+++ prolog.h
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef struct {int class;} _obj;
-typedef _obj* obj;
+struct _obj_Class;
 typedef struct _obj_Class* obj_Class;
+typedef struct {obj_Class class;} _obj;
+typedef _obj* obj;
 typedef obj (* obj_fn)();
 
-typedef struct {int class;int    in;} obj_Fixint;
-typedef struct {int class;obj  *ary;} obj_Array;
-typedef struct {int class;char *ary;} obj_String;
-typedef struct {int class;char *ary; obj_fn *methods;} _obj_Class;
+typedef struct {obj_Class class;int    in;} obj_Fixint;
+typedef struct {obj_Class class;obj  *ary;} obj_Array;
+typedef struct {obj_Class class;char *ary;} obj_String;
+typedef struct _obj_Class {obj_Class class;char *ary; obj_fn *methods;} _obj_Class;
+obj_Class class_named(char *s);
 
+obj Int(int i)    {obj_Fixint *o=malloc(sizeof(obj_Fixint));  o->class=class_named("Fixint");o->in=i;                   return (obj)o;}
+obj Array()       {obj_Array  *o=malloc(sizeof(obj_Array ));  o->class=class_named("Array" );o->ary=(obj *) malloc(100);return (obj)o;}
+obj Str(char *c  ){obj_String *o=malloc(sizeof(obj_String));  o->class=class_named("String");o->ary=c;                  return (obj)o;}
+obj Toplevel()    {_obj       *o=malloc(sizeof(_obj));        o->class=class_named("TopLevel");                         return (obj)o;}
+obj Foo()         {_obj       *o=malloc(sizeof(_obj));        o->class=class_named("Foo");                              return (obj)o;}
+obj Class(char *c){_obj_Class *o=malloc(sizeof(_obj_Class));  
+if (!strcmp(c,"Class")) o->class=o; 
+else o->class=class_named("Class"); 
 
-obj Int(int i)    {obj_Fixint *o=malloc(sizeof(obj_Fixint));  o->class=0;o->in=i;                   return (obj)o;}
-obj Array()       {obj_Array  *o=malloc(sizeof(obj_Array ));  o->class=1;o->ary=(obj *) malloc(100);return (obj)o;}
-obj Str(char *c  ){obj_String *o=malloc(sizeof(obj_String));  o->class=2;o->ary=c;                  return (obj)o;}
-obj Toplevel()    {_obj       *o=malloc(sizeof(_obj));        o->class=3;                           return (obj)o;}
-obj Foo()         {_obj       *o=malloc(sizeof(_obj));        o->class=4;                           return (obj)o;}
-obj Class(char *c){_obj_Class *o=malloc(sizeof(_obj_Class));  o->class=5;o->ary=c;o->methods=calloc(sizeof(void*),100); return (obj)o;}
+o->ary=c;o->methods=calloc(sizeof(void*),100); return (obj)o;}
 
 int   obj2int(obj o){return ((obj_Fixint*)o)->in;}
 obj*  obj2ary(obj o){return ((obj_Array *)o)->ary;}
