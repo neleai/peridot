4e4ff0588f94a90d69b89b7b26f5ad856d08b6e2
Ondrej Bilka <neleai@seznam.cz>
2012-03-03 15:57:42 +0100

    better method names

--- prolog.h
+++ prolog.h
@@ -3,12 +3,13 @@
 #include <string.h>
 typedef struct {int class;} _obj;
 typedef _obj* obj;
+typedef struct _obj_Class* obj_Class;
 typedef obj (* obj_fn)();
 
 typedef struct {int class;int    in;} obj_Fixint;
 typedef struct {int class;obj  *ary;} obj_Array;
 typedef struct {int class;char *ary;} obj_String;
-typedef struct {int class;char *ary; obj_fn *methods;} obj_Class;
+typedef struct {int class;char *ary; obj_fn *methods;} _obj_Class;
 
 
 obj Int(int i)    {obj_Fixint *o=malloc(sizeof(obj_Fixint));  o->class=0;o->in=i;                   return (obj)o;}
@@ -16,7 +17,7 @@ obj Array()       {obj_Array  *o=malloc(sizeof(obj_Array ));  o->class=1;o->ary=
 obj Str(char *c  ){obj_String *o=malloc(sizeof(obj_String));  o->class=2;o->ary=c;                  return (obj)o;}
 obj Toplevel()    {_obj       *o=malloc(sizeof(_obj));        o->class=3;                           return (obj)o;}
 obj Foo()         {_obj       *o=malloc(sizeof(_obj));        o->class=4;                           return (obj)o;}
-obj Class(char *c){obj_Class  *o=malloc(sizeof(obj_Class));   o->class=5;o->ary=c;o->methods=calloc(sizeof(void*),100); return (obj)o;}
+obj Class(char *c){_obj_Class *o=malloc(sizeof(_obj_Class));  o->class=5;o->ary=c;o->methods=calloc(sizeof(void*),100); return (obj)o;}
 
 int   obj2int(obj o){return ((obj_Fixint*)o)->in;}
 obj*  obj2ary(obj o){return ((obj_Array *)o)->ary;}
